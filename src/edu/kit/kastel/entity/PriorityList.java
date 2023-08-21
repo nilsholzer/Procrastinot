3380d12fed7c18a29be79b8f724478ec3c2b95ff
uhquw <uhquw@student.kit.edu>
2023-08-21 23:24:22 +0200

    I think i got it

--- src/edu/kit/kastel/entity/PriorityList.java
+++ src/edu/kit/kastel/entity/PriorityList.java
@@ -185,7 +185,7 @@ public class PriorityList {
             }
         }
         if (!result.isEmpty()) {
-            return result.delete(0, 2).toString();
+            return result.delete(0, 1).toString();
         }
         return "nein";
     }
